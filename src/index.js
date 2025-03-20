eddca0743b9b598306c9887d861bd667c69db43a
Daehee Lee <daehee.lee308@gmail.com>
2025-03-20 19:04:32 +0900

    weather api successful

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+const city = prompt("City name");
+
+async function getWeather(city) {
+  const key = "PVFLXPR788N76Z8BT3FLCANEY";
+  let apiData =
+    await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${key}&unitGroup=metric
+`);
+  let data = await apiData.json();
+
+  const currentConditions = data.currentConditions;
+  return currentConditions;
+}
+
+getWeather(city).then((data) => console.log(data));
